e727cde476be7a68ddad4623c497cc80a18fb4ef
BbyWiz <96n_lawrence@outlook.com>
2024-02-16 00:49:05 -0500

    New Docker push

--- Client/Program.cs
+++ Client/Program.cs
@@ -11,7 +11,7 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 //DI for the Resume link to Azure Storage
-builder.Services.AddSingleton(new BlobServiceClient(new Uri("https://lsnpersonalstorage.blob.core.windows.net/jobrelatedfiles/Lawrence%20Neris%20Software%20Engineer%20Resume.pdf?sp=r&st=2023-12-06T05:35:08Z&se=2024-12-06T13:35:08Z&spr=https&sv=2022-11-02&sr=b&sig=PpV0xNR8s7Y4VI%2BC%2BSwVd7PXYvowfPSEJm9vPJLRUmo%3D"), new DefaultAzureCredential()));
+// builder.Services.AddSingleton(new BlobServiceClient(new Uri("https://lsnpersonalstorage.blob.core.windows.net/jobrelatedfiles/Lawrence%20Neris%20Software%20Engineer%20Resume.pdf?sp=r&st=2023-12-06T05:35:08Z&se=2024-12-06T13:35:08Z&spr=https&sv=2022-11-02&sr=b&sig=PpV0xNR8s7Y4VI%2BC%2BSwVd7PXYvowfPSEJm9vPJLRUmo%3D"), new DefaultAzureCredential()));
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
